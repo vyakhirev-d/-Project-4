1d9881f9aeef103220d35040bf8cab5f12c224e0
vyakhirev-d <69190976+vyakhirev-d@users.noreply.github.com>
2021-06-24 10:11:50 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(function(){
 
-  $('.info-tabs__item').on('cklick', function(e){
+  $('.info-tabs__item').on('click', function(e){
     e.preventDefault();
     $('.info-tabs__item').removeClass('info-tabs__item--active');
     $(this).addClass('info-tabs__item--active');
+    
     $('.info-tabs__content').removeClass('.info-tabs__content--active');
     $($(this).attr('href')).addClass('.info-tabs__content--active');
   });
